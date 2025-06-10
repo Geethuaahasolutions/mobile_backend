74087e37587caf7a828197ab9535f6896621dd4a
Geethuaahasolutions <geethuaahasolutions03@gmail.com>
2025-06-10 13:30:13 +0530

    token to change passsword

--- forgot/forgotRoutes.js
+++ forgot/forgotRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const forgotController = require('./forgotController');
+const authMiddleware = require('../authMiddleware');
 const router = express.Router();
 
 router.post('/send-otp', forgotController.sendOTP);
 router.post('/verify-otp', forgotController.verifyOTP);
 router.post('/reset-password', forgotController.resetPassword);
-router.post('/change-password', forgotController.changePassword.bind(forgotController));
 
+// Protect change-password route
+router.post('/change-password', authMiddleware, forgotController.changePassword.bind(forgotController));
 
 module.exports = router;
